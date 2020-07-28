da9ac17b9b8957df50e8529676a9960e158a531a
Nidhi Kaki <kakinidhi1@gmail.com>
2020-07-28 13:50:50 -0700

    commit 1 commit 2 commit 3 commit 4

---
+++ GiftGiverMobileAppProjKWK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiftGiverMobileAppProjKWK
+//
+//  Created by Nidhi Kaki on 7/28/20.
+//  Copyright © 2020 Nidhi Kaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GiftGiverMobileAppProjKWKTests/GiftGiverMobileAppProjKWKTests.swift
@@ -0,0 +1,34 @@
+//
+//  GiftGiverMobileAppProjKWKTests.swift
+//  GiftGiverMobileAppProjKWKTests
+//
+//  Created by Nidhi Kaki on 7/28/20.
+//  Copyright © 2020 Nidhi Kaki. All rights reserved.
+//
+
+import XCTest
+@testable import GiftGiverMobileAppProjKWK
+
+class GiftGiverMobileAppProjKWKTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
